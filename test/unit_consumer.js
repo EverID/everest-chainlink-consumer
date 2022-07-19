b2845fef1bbb71a33df4985212288c6e8e734f7a
dovgopoly <idovgopoly@gmail.com>
2022-07-19 19:18:17 +0300

    And basic tests for consumer contract

---
+++ test/unit_consumer.js
@@ -0,0 +1,107 @@
+const UnitConsumer = artifacts.require("UnitConsumer");
+const Oracle = artifacts.require("Operator");
+const LinkToken = artifacts.require("LinkToken");
+
+const { constants, expectRevert } = require("@openzeppelin/test-helpers");
+const { expect } = require("chai");
+
+contract("UnitConsumer", function([owner, stranger, addr, ...accounts]) {
+    const jobId = "509e8dd8de054d3f918640ab0a2b77d8";
+    const oraclePayment = "1000000000000000000"; // 10 ** 18
+
+    beforeEach(async function () {
+        this.link = await LinkToken.new({from: owner});
+        this.oracle = await Oracle.new(this.link.address, owner, {from: owner});
+        this.consumer = await UnitConsumer.new(
+            this.link.address,
+            this.oracle.address,
+            jobId,
+            oraclePayment,
+            {from: owner}
+        );
+    });
+
+    it("should set ctor properties properly", async function () {
+        expect(this.oracle.address).to.not.be.equal(constants.ZERO_ADDRESS);
+        expect(this.consumer.address).to.not.be.equal(constants.ZERO_ADDRESS);
+
+        expect(await this.consumer.oracleAddress()).to.be.equal(this.oracle.address);
+        expect(await this.consumer.jobId()).to.be.equal(web3.utils.asciiToHex(jobId));
+        expect(await this.consumer.oraclePayment()).to.be.bignumber.equal(oraclePayment);
+        expect(await this.consumer.linkAddress()).to.be.equal(link);
+    });
+
+    describe("#setOracle", async function () {
+        it("should set properly with owner sender", async function () {
+            await this.consumer.setOracle(addr, {from: owner});
+            expect(await this.consumer.oracleAddress()).to.be.equal(addr);
+        });
+
+        it("should revert if sender is not an owner", async function () {
+            await expectRevert(
+                this.consumer.setOracle(addr, {from: stranger}),
+                'Ownable: caller is not the owner'
+            );
+        });
+    });
+
+    describe("#setOraclePayments", async function () {
+        it("should set properly with owner sender", async function () {
+            await this.consumer.setOraclePayment("1", {from: owner});
+            expect(await this.consumer.oraclePayment()).to.be.bignumber.equal("1");
+        });
+
+        it("should revert if sender is not an owner", async function () {
+            await expectRevert(
+                this.consumer.setOraclePayment("1", {from: stranger}),
+                'Ownable: caller is not the owner'
+            );
+        });
+    });
+
+    describe("#setLink", async function () {
+        it("should set properly with owner sender", async function () {
+            await this.consumer.setLink(addr, {from: owner});
+            expect(await this.consumer.linkAddress()).to.be.equal(addr);
+        });
+
+        it("should revert if sender is not an owner", async function () {
+            await expectRevert(
+                this.consumer.setLink(addr, {from: stranger}),
+                'Ownable: caller is not the owner'
+            );
+        });
+    });
+
+    describe("#setJobId", async function () {
+        const newJobId = "7223acbd01654282865b678924126013";
+        const incorrectJobId = "7223acbd01654282865b6789241260131";
+
+        it("should set properly with owner sender", async function () {
+            await this.consumer.setJobId(newJobId, {from: owner});
+            expect(await this.consumer.jobId()).to.be.equal(web3.utils.asciiToHex(newJobId));
+        });
+
+        it("should revert if sender is not an owner", async function () {
+            await expectRevert(
+                this.consumer.setJobId(newJobId, {from: stranger}),
+                'Ownable: caller is not the owner'
+            );
+        });
+
+        it("should revert if wrong invalid job id value passed", async function() {
+            await expectRevert(
+                this.consumer.setJobId(incorrectJobId, {from: owner}),
+                "Incorrect length"
+            );
+        });
+    });
+
+    describe("#statusToString", async function() {
+        it("should return correct names", async function() {
+            expect(await this.consumer.statusToString(0)).to.be.equal("undefined");
+            expect(await this.consumer.statusToString(1)).to.be.equal("kyc-user");
+            expect(await this.consumer.statusToString(2)).to.be.equal("human-unique");
+        })
+    });
+});
