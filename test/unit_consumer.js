802e60c6a09101ce3ceca5942796a4bee7f6a0fe
dovgopoly <idovgopoly@gmail.com>
2022-07-26 19:12:33 +0300

    Init test for request-fulfill flow

--- test/unit_consumer.js
+++ test/unit_consumer.js
@@ -2,10 +2,11 @@ const UnitConsumer = artifacts.require("UnitConsumer");
 const Oracle = artifacts.require("Operator");
 const LinkToken = artifacts.require("LinkToken");
 
-const { constants, expectRevert } = require("@openzeppelin/test-helpers");
+const { constants, expectRevert, expectEvent, BN } = require("@openzeppelin/test-helpers");
 const { expect } = require("chai");
+const { oracle } = require("@chainlink/test-helpers");
 
-contract("UnitConsumer", function([owner, stranger, addr, ...accounts]) {
+contract("UnitConsumer", function([owner, stranger, revealer, revealee, node, ...accounts]) {
     const jobId = "509e8dd8de054d3f918640ab0a2b77d8";
     const oraclePayment = "1000000000000000000"; // 10 ** 18
 
@@ -60,14 +61,15 @@ contract("UnitConsumer", function([owner, stranger, addr, ...accounts]) {
     });
 
     describe("#setLink", async function () {
+        const mockedNewLinkAddress = "0xa36085F69e2889c224210F603D836748e7dC0088";
         it("should set properly with owner sender", async function () {
-            await this.consumer.setLink(addr, {from: owner});
-            expect(await this.consumer.linkAddress()).to.be.equal(addr);
+            await this.consumer.setLink(mockedNewLinkAddress, {from: owner});
+            expect(await this.consumer.linkAddress()).to.be.equal(mockedNewLinkAddress);
         });
 
         it("should revert if sender is not an owner", async function () {
             await expectRevert(
-                this.consumer.setLink(addr, {from: stranger}),
+                this.consumer.setLink(mockedNewLinkAddress, {from: stranger}),
                 'Ownable: caller is not the owner'
             );
         });
@@ -89,7 +91,7 @@ contract("UnitConsumer", function([owner, stranger, addr, ...accounts]) {
             );
         });
 
-        it("should revert if wrong invalid job id value passed", async function() {
+        it("should revert if wrong invalid job id value passed", async function () {
             await expectRevert(
                 this.consumer.setJobId(incorrectJobId, {from: owner}),
                 "Incorrect length"
@@ -97,12 +99,49 @@ contract("UnitConsumer", function([owner, stranger, addr, ...accounts]) {
         });
     });
 
-    describe("#statusToString", async function() {
-        it("should return correct names", async function() {
+    describe("#statusToString", async function () {
+        it("should return correct names", async function () {
             expect(await this.consumer.statusToString(0)).to.be.equal("undefined");
             expect(await this.consumer.statusToString(1)).to.be.equal("kyc-user");
             expect(await this.consumer.statusToString(2)).to.be.equal("human-unique");
             expect(await this.consumer.statusToString(3)).to.be.equal("not-found");
-        })
+        });
+    });
+
+    // TODO: expand the number of test cases
+    describe("#requestStatus #fullfil #lastRequestId", async function () {
+        it("should set status correctly", async function () {
+            await this.oracle.setAuthorizedSenders([node], {from: owner});
+
+            await this.link.transfer(revealer, oraclePayment, {from: owner});
+            await this.link.approve(this.consumer.address, oraclePayment, {from: revealer});
+
+            const fulfillTx = await this.consumer.requestStatus(revealee, {from: revealer});
+            const request = oracle.decodeRunRequest(fulfillTx.receipt.rawLogs?.[4]);
+
+            const status = "1";
+            const kycTimestamp = "1658845449";
+
+            const responseTypes = ["uint8", "uint256"];
+            const responseValues = [status, kycTimestamp];
+
+            await this.oracle.fulfillOracleRequest2(
+                ...oracle.convertFulfill2Params(request, responseTypes, responseValues, {from: node}),
+            );
+
+            const fulfilledRequest = await this.consumer.getLastRequestId({from: revealer});
+
+            console.log(fulfilledRequest)
+            // [
+            //     '1658845449',
+            //     '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef',
+            //     '0x821aEa9a577a9b44299B9c15c88cf3087F3b5544',
+            //     '1',
+            //     kycTimestamp: '1658845449',
+            //     revealer: '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef',
+            //     revealee: '0x821aEa9a577a9b44299B9c15c88cf3087F3b5544',
+            //     status: '1'
+            // ]
+        });
     });
 });
