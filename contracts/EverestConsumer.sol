3bc1160ba2546527f4277cd941bcd4f503d0d007
dovgopoly <idovgopoly@gmail.com>
2022-11-03 14:40:17 +0200

    Change tab size

--- contracts/EverestConsumer.sol
+++ contracts/EverestConsumer.sol
@@ -9,179 +9,184 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./IEverestConsumer.sol";
 
 contract EverestConsumer is IEverestConsumer, ChainlinkClient, Ownable {
-  using Chainlink for Chainlink.Request;
-  using SafeERC20 for IERC20;
+    using Chainlink for Chainlink.Request;
+    using SafeERC20 for IERC20;
 
-  uint40 private constant OPERATOR_EXPIRATION_TIME = 5 minutes;
+    uint40 private constant OPERATOR_EXPIRATION_TIME = 5 minutes;
 
-  // latest sent request id by revealer address
-  mapping(address => bytes32) public override latestSentRequestId;
+    // latest sent request id by revealer address
+    mapping(address => bytes32) public override latestSentRequestId;
 
-  // latest fulfilled request id by revealee address
-  mapping(address => bytes32) public override latestFulfilledRequestId;
+    // latest fulfilled request id by revealee address
+    mapping(address => bytes32) public override latestFulfilledRequestId;
 
-  mapping(bytes32 => Request) private _requests;
+    mapping(bytes32 => Request) private _requests;
 
-  string public override signUpURL;
-  bytes32 public override jobId;
-  uint256 public override oraclePayment;
+    string public override signUpURL;
+    bytes32 public override jobId;
+    uint256 public override oraclePayment;
 
-  event Requested(
-    bytes32 _requestId,
-    address indexed _revealer,
-    address indexed _revealee,
-    uint40 _expiration
-  );
-
-  event Fulfilled(
-    bytes32 _requestId,
-    address indexed _revealer,
-    address indexed _revealee,
-    Status _status,
-    uint40 _kycTimestamp
-  );
-
-  modifier ifRequestExists(bytes32 requestId) {
-    require(requestExists(requestId), "Request does not exist");
-    _;
-  }
-
-  constructor(
-    address _link,
-    address _oracle,
-    string memory _jobId,
-    uint256 _oraclePayment,
-    string memory _signUpURL
-  ) {
-    setChainlinkToken(_link);
-    setChainlinkOracle(_oracle);
-    jobId = stringToBytes32(_jobId);
-    oraclePayment = _oraclePayment;
-    signUpURL = _signUpURL;
-  }
-
-  function requestStatus(address _revealee) external override {
-    require(_revealee != address(0), "Revelaee should not be zero address");
-
-    IERC20(chainlinkTokenAddress()).safeTransferFrom(msg.sender, address(this), oraclePayment);
-
-    Chainlink.Request memory request = buildOperatorRequest(jobId, this.fulfill.selector);
-    request.addBytes("address", abi.encode(_revealee));
-
-    bytes32 requestId = sendOperatorRequest(request, oraclePayment);
-
-    uint40 expiration = uint40(block.timestamp) + OPERATOR_EXPIRATION_TIME;
-    _requests[requestId] = Request({
-      isFulfilled: false,
-      isCanceled: false,
-      isHumanAndUnique: false,
-      isKYCUser: false,
-      revealer: msg.sender,
-      revealee: _revealee,
-      kycTimestamp: 0,
-      expiration: expiration
-    });
-    latestSentRequestId[msg.sender] = requestId;
-
-    emit Requested(requestId, msg.sender, _revealee, expiration);
-  }
-
-  function fulfill(
-    bytes32 _requestId,
-    Status _status,
-    uint40 _kycTimestamp
-  ) external override recordChainlinkFulfillment(_requestId) {
-    if (_status == Status.KYCUser) {
-      require(_kycTimestamp != 0, "_kycTimestamp should not be zero for KYCUser");
-    } else {
-      require(_kycTimestamp == 0, "_kycTimestamp should be zero for non-KYCUser");
-    }
-
-    Request storage request = _requests[_requestId];
-    request.kycTimestamp = _kycTimestamp;
-    request.isFulfilled = true;
-    request.isHumanAndUnique = _status != Status.NotFound;
-    request.isKYCUser = _status == Status.KYCUser;
-    latestFulfilledRequestId[request.revealee] = _requestId;
-
-    emit Fulfilled(_requestId, request.revealer, request.revealee, _status, _kycTimestamp);
-  }
-
-  function cancelRequest(bytes32 _requestId) external override ifRequestExists(_requestId) {
-    Request storage request = _requests[_requestId];
-    require(
-      !request.isCanceled && !request.isFulfilled,
-      "Request should not be canceled or fulfilled"
+    event Requested(
+        bytes32 _requestId,
+        address indexed _revealer,
+        address indexed _revealee,
+        uint40 _expiration
     );
-    require(request.revealer == msg.sender, "You are not an owner of the request");
-    cancelChainlinkRequest(_requestId, oraclePayment, this.fulfill.selector, request.expiration);
-    IERC20(chainlinkTokenAddress()).safeTransfer(msg.sender, oraclePayment);
-    request.isCanceled = true;
-  }
 
-  function getRequest(
-    bytes32 _requestId
-  ) public view override ifRequestExists(_requestId) returns (Request memory) {
-    return _requests[_requestId];
-  }
+    event Fulfilled(
+        bytes32 _requestId,
+        address indexed _revealer,
+        address indexed _revealee,
+        Status _status,
+        uint40 _kycTimestamp
+    );
 
-  function getLatestFulfilledRequest(
-    address _revealee
-  ) external view override returns (Request memory) {
-    return getRequest(latestFulfilledRequestId[_revealee]);
-  }
-
-  function setSignUpURL(string memory _signUpURL) external override onlyOwner {
-    signUpURL = _signUpURL;
-  }
-
-  function getLatestSentRequestId() external view override returns (bytes32) {
-    require(latestSentRequestId[msg.sender] != 0, "No requests yet");
-
-    return latestSentRequestId[msg.sender];
-  }
-
-  function requestExists(bytes32 _requestId) public view override returns (bool) {
-    return _requests[_requestId].revealer != address(0);
-  }
-
-  function statusToString(Status _status) external pure override returns (string memory) {
-    if (_status == Status.KYCUser) {
-      return "KYC_USER";
+    modifier ifRequestExists(bytes32 requestId) {
+        require(requestExists(requestId), "Request does not exist");
+        _;
     }
-    if (_status == Status.HumanAndUnique) {
-      return "HUMAN_AND_UNIQUE";
+
+    constructor(
+        address _link,
+        address _oracle,
+        string memory _jobId,
+        uint256 _oraclePayment,
+        string memory _signUpURL
+    ) {
+        setChainlinkToken(_link);
+        setChainlinkOracle(_oracle);
+        jobId = stringToBytes32(_jobId);
+        oraclePayment = _oraclePayment;
+        signUpURL = _signUpURL;
     }
-    return "NOT_FOUND";
-  }
 
-  function setOracle(address _oracle) external override onlyOwner {
-    setChainlinkOracle(_oracle);
-  }
+    function requestStatus(address _revealee) external override {
+        require(_revealee != address(0), "Revelaee should not be zero address");
 
-  function setLink(address _link) external override onlyOwner {
-    setChainlinkToken(_link);
-  }
+        IERC20(chainlinkTokenAddress()).safeTransferFrom(msg.sender, address(this), oraclePayment);
 
-  function setOraclePayment(uint256 _oraclePayment) external override onlyOwner {
-    oraclePayment = _oraclePayment;
-  }
+        Chainlink.Request memory request = buildOperatorRequest(jobId, this.fulfill.selector);
+        request.addBytes("address", abi.encode(_revealee));
 
-  function setJobId(string memory _jobId) external override onlyOwner {
-    jobId = stringToBytes32(_jobId);
-  }
+        bytes32 requestId = sendOperatorRequest(request, oraclePayment);
 
-  function oracleAddress() external view override returns (address) {
-    return chainlinkOracleAddress();
-  }
+        uint40 expiration = uint40(block.timestamp) + OPERATOR_EXPIRATION_TIME;
+        _requests[requestId] = Request({
+            isFulfilled: false,
+            isCanceled: false,
+            isHumanAndUnique: false,
+            isKYCUser: false,
+            revealer: msg.sender,
+            revealee: _revealee,
+            kycTimestamp: 0,
+            expiration: expiration
+        });
+        latestSentRequestId[msg.sender] = requestId;
 
-  function linkAddress() external view override returns (address) {
-    return chainlinkTokenAddress();
-  }
+        emit Requested(requestId, msg.sender, _revealee, expiration);
+    }
 
-  function stringToBytes32(string memory _source) private pure returns (bytes32) {
-    bytes memory source = bytes(_source);
-    require(source.length == 32, "Incorrect length");
-    return bytes32(source);
-  }
+    function fulfill(
+        bytes32 _requestId,
+        Status _status,
+        uint40 _kycTimestamp
+    ) external override recordChainlinkFulfillment(_requestId) {
+        if (_status == Status.KYCUser) {
+            require(_kycTimestamp != 0, "_kycTimestamp should not be zero for KYCUser");
+        } else {
+            require(_kycTimestamp == 0, "_kycTimestamp should be zero for non-KYCUser");
+        }
+
+        Request storage request = _requests[_requestId];
+        request.kycTimestamp = _kycTimestamp;
+        request.isFulfilled = true;
+        request.isHumanAndUnique = _status != Status.NotFound;
+        request.isKYCUser = _status == Status.KYCUser;
+        latestFulfilledRequestId[request.revealee] = _requestId;
+
+        emit Fulfilled(_requestId, request.revealer, request.revealee, _status, _kycTimestamp);
+    }
+
+    function cancelRequest(bytes32 _requestId) external override ifRequestExists(_requestId) {
+        Request storage request = _requests[_requestId];
+        require(
+            !request.isCanceled && !request.isFulfilled,
+            "Request should not be canceled or fulfilled"
+        );
+        require(request.revealer == msg.sender, "You are not an owner of the request");
+        cancelChainlinkRequest(
+            _requestId,
+            oraclePayment,
+            this.fulfill.selector,
+            request.expiration
+        );
+        IERC20(chainlinkTokenAddress()).safeTransfer(msg.sender, oraclePayment);
+        request.isCanceled = true;
+    }
+
+    function getRequest(
+        bytes32 _requestId
+    ) public view override ifRequestExists(_requestId) returns (Request memory) {
+        return _requests[_requestId];
+    }
+
+    function getLatestFulfilledRequest(
+        address _revealee
+    ) external view override returns (Request memory) {
+        return getRequest(latestFulfilledRequestId[_revealee]);
+    }
+
+    function setSignUpURL(string memory _signUpURL) external override onlyOwner {
+        signUpURL = _signUpURL;
+    }
+
+    function getLatestSentRequestId() external view override returns (bytes32) {
+        require(latestSentRequestId[msg.sender] != 0, "No requests yet");
+
+        return latestSentRequestId[msg.sender];
+    }
+
+    function requestExists(bytes32 _requestId) public view override returns (bool) {
+        return _requests[_requestId].revealer != address(0);
+    }
+
+    function statusToString(Status _status) external pure override returns (string memory) {
+        if (_status == Status.KYCUser) {
+            return "KYC_USER";
+        }
+        if (_status == Status.HumanAndUnique) {
+            return "HUMAN_AND_UNIQUE";
+        }
+        return "NOT_FOUND";
+    }
+
+    function setOracle(address _oracle) external override onlyOwner {
+        setChainlinkOracle(_oracle);
+    }
+
+    function setLink(address _link) external override onlyOwner {
+        setChainlinkToken(_link);
+    }
+
+    function setOraclePayment(uint256 _oraclePayment) external override onlyOwner {
+        oraclePayment = _oraclePayment;
+    }
+
+    function setJobId(string memory _jobId) external override onlyOwner {
+        jobId = stringToBytes32(_jobId);
+    }
+
+    function oracleAddress() external view override returns (address) {
+        return chainlinkOracleAddress();
+    }
+
+    function linkAddress() external view override returns (address) {
+        return chainlinkTokenAddress();
+    }
+
+    function stringToBytes32(string memory _source) private pure returns (bytes32) {
+        bytes memory source = bytes(_source);
+        require(source.length == 32, "Incorrect length");
+        return bytes32(source);
+    }
 }
